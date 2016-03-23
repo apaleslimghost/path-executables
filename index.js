c8357e42e8fc8e4e885945c9815edabf3cc74080
Matt Brennan <mattyb1000@gmail.com>
2016-03-23 09:24:56 +0000

    feat: return a promise
    BREAKING CHANGE: no longer uses callbacks

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+var Promise = require('any-promise');
+var fs = require('fs-promise');
+var flatten = require('lodash.flatten');
+var pathJoin = require('path').join;
 var PATH = process.env.PATH;
-var fs = require('fs');
-var asyncFlatMap = require('async-flat-map');
 
 function getPaths(path) {
 	return path.split(':');
 }
 
-var listAll = asyncFlatMap(fs.readdir);
-
-module.exports = listAll(getPaths(PATH));
+module.exports = function(path) {
+	return Promise.all(getPaths(path || PATH).map(function(p) {
+		return fs.readdir(p).then(function(execs) {
+			return execs.map(function(exec) {
+				return pathJoin(p, exec);
+			});
+		});
+	})).then(flatten);
+};
